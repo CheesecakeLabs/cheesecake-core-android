ade5f73b78d686b81ba8ec5210034f1074c2b6ad
gianebinotto <giane.alessio@gmail.com>
2020-03-03 15:31:13 -0300

    Refactor: remove unused imports

--- core/src/androidTest/java/io/cheesecakelabs/core/ExampleInstrumentedTest.java
+++ core/src/androidTest/java/io/cheesecakelabs/core/ExampleInstrumentedTest.java
@@ -3,7 +3,6 @@ package io.cheesecakelabs.core;
 import android.content.Context;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
